0cf45a2cfa8961dcb8bd6e5e318218802e32d86e
marci1175 <v.marcell4000@gmail.com>
2026-03-09 23:37:25 +0100

    yeup

--- test_project/src/main.f
+++ test_project/src/main.f
@@ -1,5 +1,4 @@
 external printf(str: string, ...): int;
-external getchar(): int;
 
 struct marci {
     a: int,    
@@ -10,12 +9,6 @@ trait majom {
 }
 
 marci implements {
-    pub function get_num(this, mul: int): int {
-        this.a = 900;
-
-        return this.a * mul;
-    }
-
     pub function beszel(this): int {
         printf("Szia batyus helyzeto: %i", this.a);
 
@@ -23,7 +16,7 @@ marci implements {
     }
 }
 
-pub function add |T <- majom| (lhs: T): int {
+pub function beszeltet |T <- majom| (lhs: T): int {
     int a = lhs.beszel();
     return a;
 }
@@ -31,12 +24,7 @@ pub function add |T <- majom| (lhs: T): int {
 pub function main(): int {
     marci q = marci { a: 10 };
 
-    # This shit does NOT work, llvm is doing something shady here lol pls investigate codegen
-    # Must be some sort of a memory issue
-    # Fn isnt called when not stored and is with incorrect arguments inside
-    # int a = q.beszel();
-
-    add(q);
+    beszeltet(q);
 
     return 0;
 }
