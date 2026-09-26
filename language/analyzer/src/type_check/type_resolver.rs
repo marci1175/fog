7c49afbc8c82a7178dae9efad02910f052e5852c
marci1175 <v.marcell4000@gmail.com>
2026-09-26 13:25:07 +0200

    fixed type resolver

--- language/analyzer/src/type_check/type_resolver.rs
+++ language/analyzer/src/type_check/type_resolver.rs
@@ -1,6 +1,26 @@
 use common::{
-    anyhow, codegen::ty::{Type, Value}, error::analyzer::AnalyzerError,
+    anyhow, codegen::ty::{NotNan, Type, Value}, error::analyzer::AnalyzerError,
 };
+
+#[macro_export]
+macro_rules! create_conversion_chain {
+    {$destination_ty_var:ident, $modifyable_value:ident, $raw_value:ident, $original_value_ty:ty, $(($destination_ty:ident, $destination_value_ty:ty),)*} => {
+        $(
+            if $destination_ty_var == Type::$destination_ty {
+                match <$destination_value_ty>::try_from($raw_value) {
+                    Ok(converted_val) => {
+                        *$modifyable_value = Value::$destination_ty(converted_val);
+                    }
+                    Err(_) => {
+                        return Err(AnalyzerError::UnsupportedValueByType($modifyable_value.clone(), $destination_ty_var).into());
+                    }
+                }
+                
+                return Ok(());
+            }
+        )*
+    };
+}
 
 /// This function is only ran on values which do not have a type cast. (ie. the user did not specifically cast the value to a specific type.)
 pub fn resolve_numerical_size(dest_ty: Type, value: &mut Value) -> anyhow::Result<()>
@@ -13,40 +33,126 @@ pub fn resolve_numerical_size(dest_ty: Type, value: &mut Value) -> anyhow::Resul
         return Err(AnalyzerError::InternalValueNotNumerical(original_ty).into());
     }
 
+    // If the original type matches the desitnation type then we dont have to do anything really, we can just return Ok
+    if original_ty == dest_ty {
+        return Ok(());
+    }
+
     // Only the numerical value's size needs to be figured out as the numbers type is resolved unlike their size. (Except int to uint) 
     match value.clone() {
         Value::F64(_) => {
             // If a value is parsed as an f64 it cannot be casted to anything else without data loss.
             if original_ty != dest_ty {
-                return Err(AnalyzerError::InvalidAutomaticTypeSizeResolve(value.clone(), dest_ty).into());
+                return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
             }
         }
         Value::U64(val) => {
-            if dest_ty.is_int() {
-                // Modify the value passed in
-                *value = Value::I64(val as i64);
+            create_conversion_chain! {
+                dest_ty, value, val, u64,
+                (I64, i64),
+            };
+
+            // The macro produces code that should automatically return, so if the code executes favourably this error will never be returned.
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+        }
+        Value::F32(val) => {
+            if dest_ty == Type::F64 {
+                // Cast the inner value (this cannot really return an error since we are already dereferencing a type that is NotNan<T>)
+                *value = Value::F64(NotNan::new(*val as f64)?);
 
                 return Ok(());
             }
-            else {
-                return Err(AnalyzerError::InvalidAutomaticTypeSizeResolve(value.clone(), dest_ty).into());
-            }
+
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
         }
-        Value::F32(val) => {}
-        Value::U32(val) => {}
-        Value::F16(val) => {}
-        Value::U16(val) => {}
-        Value::U8(val) => {}
+        Value::U32(val) => {
+            create_conversion_chain! {
+                dest_ty, value, val, u32,
+                (I32, i32),
+                (I64, i64),
+                (U64, u64),
+            };
+
+            // The macro produces code that should automatically return, so if the code executes favourably this error will never be returned.
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+        }
+        Value::F16(val) => {
+            if dest_ty == Type::F64 {
+                // Cast the inner value (this cannot really return an error since we are already dereferencing a type that is NotNan<T>)
+                *value = Value::F64(NotNan::new(*val as f64)?);
+
+                return Ok(());
+            }
+            else if dest_ty == Type::F32 {
+                // Cast the inner value (this cannot really return an error since we are already dereferencing a type that is NotNan<T>)
+                *value = Value::F32(NotNan::new(*val as f32)?);
+
+                return Ok(());
+            }
+
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+        }
+        Value::U16(val) => {
+            create_conversion_chain! {
+                dest_ty, value, val, u16,
+                (I16, i16),
+                (I32, i32),
+                (I64, i64),
+                (U32, u32),
+                (U64, u64),
+            };
+
+            // The macro produces code that should automatically return, so if the code executes favourably this error will never be returned.
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+        }
+        Value::U8(val) => {
+            create_conversion_chain! {
+                dest_ty, value, val, u8,
+                (I16, i16),
+                (I32, i32),
+                (I64, i64),
+                (U16, u16),
+                (U32, u32),
+                (U64, u64),
+            };
+
+            // The macro produces code that should automatically return, so if the code executes favourably this error will never be returned.
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+        }
 
         // These are usually not emitted by the AST parser.
         // Negative numbers are parsed as NegateValue(uint)
-        Value::I64(val) => return Ok(()),
-        Value::I32(val) => {}
-        Value::I16(val) => {}
+        Value::I64(_) => {
+            if original_ty != dest_ty {
+                return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+            }
+        },
+        Value::I32(val) => {
+            create_conversion_chain! {
+                dest_ty, value, val, i32,
+                (I64, i64),
+                (U64, u64),
+                (U32, u32),
+            };
+
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+        }
+        Value::I16(val) => {
+            create_conversion_chain! {
+                dest_ty, value, val, i16,
+                (I64, i64),
+                (I32, i32),
+                (U64, u64),
+                (U32, u32),
+                (U16, u16),
+            };
+
+            return Err(AnalyzerError::UnsupportedValueByType(value.clone(), dest_ty).into());
+        }
 
         _ => return Err(AnalyzerError::InternalValueNotNumerical(original_ty).into()),
     }
 
-
+    // No modification has been done to the passed in value, all passed without issue.
     Ok(())
 }
--- language/common/src/error/analyzer.rs
+++ language/common/src/error/analyzer.rs
@@ -5,8 +5,8 @@ use crate::codegen::ty::{Type, Value};
 #[derive(Debug, Error)]
 pub enum AnalyzerError
 {
-    #[error("Value `{0}` cannot be automatically casted to type `{1}` without data loss. Consider casting with the `as` keyword.")]
-    InvalidAutomaticTypeSizeResolve(Value, Type),
+    #[error("The value `{0}` cannot be fit into the destination type `{1}`.")]
+    UnsupportedValueByType(Value, Type),
     #[error("[INTERNAL ERROR] Value with type `{0}` is not numerical. Check where the automatic type")]
     InternalValueNotNumerical(Type),
 }
